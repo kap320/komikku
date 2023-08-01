d2ae8ea7925fbbf23b7d70c0ad07c5e834646de7
ZeroneDoo <rizqipambudi6@gmail.com>
2023-08-01 08:36:40 +0700

    api url

--- src/Network/api.jsx
+++ src/Network/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://komikku-api.zeronedoo.repl.co"
 
 export const getComic = async () => {
     const response = await axios.get(BASE_URL + "/api/comic/popular/page/1")
