5245a4df2326eeacb56eb3bf87576cd9846774e3
shanachan <77118749+kap320@users.noreply.github.com>
2024-01-15 09:35:46 +0700

    Update api.jsx

--- src/Network/api.jsx
+++ src/Network/api.jsx
@@ -1,28 +1,28 @@
 import axios from "axios"
 
-const BASE_URL = "https://zeronewatch-api.vercel.app"
+const BASE_URL = "https://manga-api-kappa.vercel.app"
 
 export const getComic = async () => {
-    const response = await axios.get(BASE_URL + "/api/comic/popular/page/1")
+    const response = await axios.get(BASE_URL + "/api/popular/page/1")
     return response.data
 }
 
 export const getTopComic = async () => {
-    const response = await axios.get(BASE_URL + "/api/comic/recommended/page/1")
+    const response = await axios.get(BASE_URL + "/api/recommended/page/1")
     return response.data
 }
 
 export const detailComic = async (endpoint) => {
-    const response = await axios.get(BASE_URL + "/api/comic/info/" + endpoint)
+    const response = await axios.get(BASE_URL + "/api/genres/" + endpoint)
     return response.data
 }
 
 export const detailChapter = async (endpoint) => {
-    const response = await axios.get(BASE_URL + '/api/comic/chapter/ch/' + endpoint)
+    const response = await axios.get(BASE_URL + '/api/chapter/' + endpoint)
     return response.data
 }
 
 export const searchComic = async (key) => {
-    const response = await axios.get(BASE_URL + "/api/comic/search/" + key)
+    const response = await axios.get(BASE_URL + "/api/search/" + key)
     return response.data
 }
